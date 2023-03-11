a75ba424989babe5bcaf72679dddb86506422c6a
HOMECOMPUTER <106763952+somedudeidk3984@users.noreply.github.com>
2023-03-11 13:36:00 -0600

    finished menu
    basically finished game

--- AM-GravityTrails/Assets/Scripts/Teleport.cs
+++ AM-GravityTrails/Assets/Scripts/Teleport.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 public class Teleport : MonoBehaviour
 {
     public int enemyCount;
+    public MeshRenderer mr;
 
     // Start is called before the first frame update
     void Start()
@@ -16,14 +17,21 @@ public class Teleport : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (enemyCount > 0)
+        {
+            mr.enabled = false;
+        }
+        else
+        {
+            mr.enabled = true;
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.tag == "Player" && enemyCount == 0)
         {
-            SceneManager.LoadScene(1);
+            SceneManager.LoadScene(2);
         }
     }
 }
